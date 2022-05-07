39a3e92611bff2a6deb2aa08e6b3421108002ccd
Ravi Singh <ravi@appsimpact.com>
2022-05-07 22:50:21 +0530

    working with charge and token apis

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -18,4 +18,33 @@ class StripeService
     end
     stripe_customer
   end
+
+  def create_card_token(params)
+    Stripe::Token.create({
+      card: {
+        number: params[:card_number].to_s,
+        exp_month: params[:exp_month],
+        exp_year: params[:exp_year],
+        cvc: params[:cvv]
+      }
+    })
+  end
+
+  def create_stripe_customer_card(params, stripe_customer)
+    token = create_card_token(params)
+    Stripe::Customer.create_source(
+      stripe_customer.id,
+      { source: token.id }
+    )
+  end
+
+  def create_stripe_charge(amount_to_be_paid, stripe_customer_id, card_id, workshop)
+    Stripe::Charge.create({
+      amount: amount_to_be_paid * 100,
+      currency: 'usd',
+      source: card_id,
+      customer: stripe_customer_id,
+      description: "Amount $#{amount_to_be_paid} charged for #{workshop.name}"
+    })
+  end
 end
