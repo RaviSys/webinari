c9029abb7f74959c97e0927083c815663a6fda25
Ravi Singh <ravi@appsimpact.com>
2022-07-09 19:15:19 +0530

    working on refund successnotification

--- app/mailers/refund_notification_mailer.rb
+++ app/mailers/refund_notification_mailer.rb
@@ -17,4 +17,20 @@ class RefundNotificationMailer < ApplicationMailer
     @refundable_amount = @refund.no_of_tickets * @workshop.registration_fee
     mail to: AdminUser.first.email, subject: "Refund request coming for #{@workshop.name}"
   end
+
+  def refund_success_notification_to_customer(refund)
+    @refund = refund
+    @booking = refund.booking
+    @customer = refund.customer
+    @workshop = @booking.workshop
+    mail to: @customer.email, subject: "Refund processed successfully for #{@workshop.name}"
+  end
+
+  def refund_success_notification_to_admin(refund)
+    @refund = refund
+    @booking = refund.booking
+    @customer = refund.customer
+    @workshop = @booking.workshop
+    mail to: AdminUser.first.email, subject: "Thank you for processing refund for #{@workshop.name} | #{@booking.order_number}"
+  end
 end
--- app/models/refund.rb
+++ app/models/refund.rb
@@ -3,6 +3,18 @@ class Refund < ApplicationRecord
   belongs_to :booking
 
   def amount_to_be_refunded
-    no_of_tickets * booking.workshop.registration_fee
+    no_of_tickets * ticket_fee
+  end
+
+  def remaining_tickets_for_refund
+    booking.no_of_tickets - no_of_tickets
+  end
+
+  def remaining_amount_for_refund
+    remaining_tickets_for_refund * ticket_fee
+  end
+
+  def ticket_fee
+    booking.workshop.registration_fee
   end
 end
