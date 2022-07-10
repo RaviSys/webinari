e8bb8293c17bd9b64a215bf085e81f6dca1f58d7
Ravi Singh <ravi@appsimpact.com>
2022-07-10 11:35:52 +0530

    worked on bookings index in admin

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -1,6 +1,10 @@
 class Admin::BookingsController < AdminController
   before_action :set_booking_with_parents, only: %i[show]
 
+  def index
+    @bookings = Booking.includes(:customer, :workshop)
+  end
+
   def show
   end
 
