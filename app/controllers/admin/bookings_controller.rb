0fd6f244cfc4c0cab63e1729d54782f9aeb32452
Ravi Singh <ravi@appsimpact.com>
2022-07-10 13:45:10 +0530

    search form for bookings index

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -1,11 +1,17 @@
 class Admin::BookingsController < AdminController
-  before_action :set_booking_with_parents, only: %i[show]
+  before_action :set_booking_with_parents, only: %i[show destroy]
 
   def index
-    @bookings = Booking.includes(:customer, :workshop)
+    @q = Booking.ransack(params[:q])
+    @bookings = @q.result.includes(:customer, :workshop)
   end
 
   def show
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to admin_bookings_path, notice: 'Booking deleted successfully'
   end
 
   private
